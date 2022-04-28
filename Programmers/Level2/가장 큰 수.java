eb60f4e9f553bb8f388a3851fd7c3882920dbd10
Song-EunJu <jd06280@naver.com>
2022-04-28 12:07:47 +0900

    가장 큰 수

--- Programmers/Level2/가장 큰 수.java
+++ Programmers/Level2/가장 큰 수.java
@@ -1,3 +1,33 @@
+//import java.util.*;
+//class Solution {
+//    public String solution(int[] numbers) {
+//        String str[] = new String[numbers.length];
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (int i = 0; i < numbers.length; i++) {
+//            str[i] = String.valueOf(numbers[i]);
+//        }
+//
+//        /*
+//            100, 34를 비교한 경우 -> 첫글자 1-3 = -2 리턴
+//            95, 94 를 비교한 경우 -> 두번째 글자 5-4=1
+//        */
+//
+//        Arrays.sort(str, new Comparator<String>() {
+//            @Override
+//            public int compare(String a, String b) {
+//                return (b + a).compareTo(a + b);
+//            }
+//        });
+//
+//        for (int i = 0; i < str.length; i++) {
+//            sb.append(str[i]);
+//        }
+//
+//        return sb.toString().charAt(0) == '0' ? "0" : sb.toString();
+//    }
+//}
+
 import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
@@ -13,12 +43,7 @@ class Solution {
             95, 94 를 비교한 경우 -> 두번째 글자 5-4=1
         */
 
-        Arrays.sort(str, new Comparator<String>(){
-            @Override
-            public int compare(String a, String b){
-                return (b+a).compareTo(a+b);
-            }
-        });
+        Arrays.sort(str, (a,b) -> (b+a).compareTo(a+b));
 
         for(int i=0;i<str.length;i++){
             sb.append(str[i]);
@@ -26,3 +51,4 @@ class Solution {
 
         return sb.toString().charAt(0)=='0'?"0":sb.toString();
     }
+}
