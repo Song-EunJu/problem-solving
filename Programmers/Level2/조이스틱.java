81a6f610d517c19d744508ae75eec19a3e6307e8
Song-EunJu <jd06280@naver.com>
2022-10-09 01:35:46 +0900

    221009: 조이스틱_탐욕법

--- Programmers/Level2/조이스틱.java
+++ Programmers/Level2/조이스틱.java
@@ -1,10 +1,20 @@
 class Solution {
     public int solution(String name) {
         int sum=0;
-        int min = name.length()-1;
+        int min = name.length()-1; // 좌우로 움직이는 가장 짧은 경우는 그냥 오른쪽으로 1칸씩 끝까지 가는 방법
         for(int i=0;i<name.length();i++){
-            sum+=Math.min(name.charAt(i)-'A', 'Z'-name.charAt(i)+1);  // 상하로 굴리기
+            char ch = name.charAt(i);
+            sum+=Math.min(ch-'A', 'Z'-ch+1);  // 상하로 굴리기
 
+            /**
+             * 모든 문자마다, i=현재위치. index=현재위치 이후에 A가 아닌 문자가 나오는 첫 위치 (다음에 방문할 index 위치) 구함
+             * 
+             * 목표 : 현재 위치에서 다음에 방문할 index 위치까지 움직이는 거리 계산하기
+             * 배열의 0번 인덱스 지점부터 현위치까지 왔다가 다시 왼쪽으로 돌아가서 index 위치까지 가는 길이 빠른지
+             * 배열의 0번 인덱스 지점에서 왼쪽으로 가서 index 위치에 먼저 갔다가 현위치로 오는게 빠른지
+             * 
+             * 이 연산을 모든 문자마다 하면서, 전체 문자열에서 좌우 이동거리가 가장짧은 구간을 구함
+             */
             int index = i+1;
             while(index<name.length() && name.charAt(index)=='A')
                 index++;
@@ -26,3 +36,28 @@ class Solution {
         return sum;
     }
 }
+
+
+// class Solution {
+//     public int solution(String name) {
+//         int i=0;
+//         int sum=0;
+//         while(true){
+//             int j=0;
+//             if(name.charAt(i)!='A')
+//                 sum+=Math.min(name.charAt(i)-'A', 'Z'-name.charAt(i)+1);
+//             if(i>=name.length()-1)
+//                 break;
+//             for(j=i+1;j<name.length();j++){
+//                 if(name.charAt(j)!='A')
+//                     break;
+//             }
+//             // 왼, 오른쪽 중 어디로 가는게 더 빠른지 더하기
+//             sum+=Math.min(j-i, name.length()-j);
+//             // A가 아닌 알파벳 인덱스로 넘어가기
+//             i+=(j-i);
+//         }
+//         return sum;
+//     }
+// }
+
--- src/baekjoon14888.java
+++ src/baekjoon14888.java
@@ -1,2 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * [문제 이해]
+ * - 수의 순서 변경 불가
+ * - 식의 계산은 앞에서 부터 진행
+ * - 식의 결과가 최대, 최소인 것 구하기
+ * */
 public class baekjoon14888 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int number[] = new int[n];
+        int op[] = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            op[i] = Integer.parseInt(st.nextToken());
+        }
+
+        calculate(number, op);
+    }
+    public static void calculate(int number[], int op[]){
+        int sum = number[0];
+        for(int i=1;i<number.length;i++) {
+            int nextNum = number[i];
+
+            switch(nextNum){
+                case 0:
+                    sum += nextNum;
+                    break;
+                case 1:
+                    sum -= nextNum;
+                    break;
+                case 2:
+                    sum *= nextNum;
+                    break;
+                case 3:
+                    sum /= nextNum;
+                    break;
+            }
+
+
+        }
+
+    }
 }
