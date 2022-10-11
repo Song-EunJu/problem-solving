b988e8c4255eaa494099928d5740ec0713190161
Song-EunJu <jd06280@naver.com>
2022-10-11 21:00:43 +0900

    221011: 쿼드압축 후 개수 세기_재귀

--- Programmers/Level2/쿼드압축 후 개수 세기.java
+++ Programmers/Level2/쿼드압축 후 개수 세기.java
@@ -1,4 +1,39 @@
 class Solution {
+    static int zero = 0;
+    static int one = 0;
+
+    public int[] solution(int[][] arr) {
+        cal(0,0,arr.length, arr);
+        return new int[]{zero, one};
+    }
+
+    public void cal(int a, int b, int n, int[][] arr){
+        int sum = 0;
+        // 전체 이중 for문을 돌면서, sum값을 계산한다
+        for(int i=a;i<a+n;i++){
+            for(int j=b;j<b+n;j++){
+                sum+=arr[i][j];
+            }
+        }
+
+        // 모든 수가 0이 아니거나, 1이 아닌 경우 4등분으로 쪼개야 함~
+        if(sum!=0 && sum!=n*n){
+            cal(a, b, n/2, arr);
+            cal(a, b+n/2, n/2, arr);
+            cal(a+n/2, b, n/2, arr);
+            cal(a+n/2, b+n/2, n/2, arr);
+        }
+        else if(sum==0 || sum==n*n){
+            if(sum==0)
+                zero++;
+            else
+                one++;
+        }
+    }
+}
+
+
+class Solution {
     static int zero=0, one=0;
     public int[] solution(int[][] arr) {
         int num = arr.length;
