cb78f4cfda9df1d24d7e16fd20addd6fb9699eec
Song-EunJu <jd06280@naver.com>
2022-10-11 20:42:29 +0900

    221011: 이진 변환 반복하기_구현

--- Programmers/Level2/이진 변환 반복하기.java
+++ Programmers/Level2/이진 변환 반복하기.java
@@ -1,6 +1,30 @@
 class Solution {
     public int[] solution(String s) {
         int zero=0;
+        int cnt=0;
+        int sum=0;
+        while(s.length()!=1){
+
+            // 문자열에서 0 개수를 세고
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i)=='0')
+                    zero++;
+            }
+
+            // 현재 문자열에서 0 개수를 다 뺀 만큼의 수를 2진수로 만든다
+            int num = s.length()-zero;
+            s = Integer.toBinaryString(num);
+            cnt++; // 이진 변환의 횟수
+            sum+=zero; // 매 변환과정마다 제거되는 0의 갯수의 합계
+            zero = 0;
+        }
+        return new int[]{cnt, sum};
+    }
+}
+
+class Solution {
+    public int[] solution(String s) {
+        int zero=0;
         int count=0;
         while(s.length()!=1){
             int orgCount = s.length();
