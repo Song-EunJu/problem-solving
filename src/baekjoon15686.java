eb3891ec3ed22d38d77592b7f9da36e60fd0d7e5
Song-EunJu <jd06280@naver.com>
2022-10-15 14:47:51 +0900

    221015: 백준 15686번_백트래킹

--- src/baekjoon15686.java
+++ src/baekjoon15686.java
@@ -3,148 +3,93 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
-/**
- * 1. 폐업시키지 않을 치킨집 고르기
- * 2. 폐업시킬 치킨집은 0으로 바꾸기
- * 3. for문을 돌면서 값이 1인 애가 나오면, 그 자리에서 가장 가까운 2와의 거리를 구한다.
- *    거리를 구하면서 다 더해서 치킨거리 최솟값 출력하기
- *
- * 아 이렇게 하면 안되나 ...
- * 1. 2가 나오는 순간, 해당 2와의 거리를 다 더한다. (1인 원소들이)
- * 2. 그리고 고놈들이 가장 작은 순서대로 m개 빼고 다 0으로 바꾼다.
- * 3. 그리고 1이 나오면 가장 가까운 2를 찾아서 더한다.
- *
- */
 class Location {
     int y;
     int x;
-    int min;
 
-    public Location(int y, int x, int sum) {
+    public Location(int y, int x) {
         this.y = y;
         this.x = x;
-        this.min = sum;
     }
 }
 
 public class baekjoon15686 {
-    static int dy[] = {-1, 1, 0, 0};
-    static int dx[] = {0, 0, -1, 1};
-    /**
-     * dy dx
-     * -1 0 상
-     * 1 0 하
-     * 0 -1 좌
-     * 0 1 우
-     */
     static int n;
     static int m;
     static int arr[][];
     static List<Location> chickens;
+    static List<Location> houses;
+    static boolean visited[];
+
+    static int finalSum = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        // 폐업시키지 않을 치킨집 최대 M개 - M개만 남기고 다 0으로 만들면 됨.
-
         arr = new int[n][n];
+
+        /**
+         * 폐업시키지 않을 애들부터 정하고 치킨거리를 구해야 하지 않을까 -> 맞음. for문 돌면서 하나씩 방문처리하고
+         * 최소 (N-M)개의 치킨집을 없애버린다 == 0으로 만든다. -> 이럴 필요 없이 치킨집의 좌표만 리스트에 저장해두면 됨
+         * N-M 개보다는 큰 범위 내에서 하나씩 폐업시킨다 -> 치킨집 좌표만 리스트에 저장해두고 방문처리하면서 값이 m과 동일할 때 계산 시작
+         * 각집에서 가장 가까운 치킨집은 어떻게 구하는가 -> 이중 포문으로 돈다
+         * -> 집이 나올때마다 BFS?
+         *
+         * */
+
+        houses = new ArrayList<>();
         chickens = new ArrayList<>();
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<n;j++){
+            for (int j = 0; j < n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
+                if(arr[i][j] == 1) // 집
+                    houses.add(new Location(i,j));
+                else if(arr[i][j]  == 2) // 치킨집
+                    chickens.add(new Location(i,j));
             }
         }
-        ////////// 값 세팅
-        chooseChicken();
 
-        // 최대 M개
-        for(int i=m;i<chickens.size();i++){ // m번 초과의 치킨집은 0으로 만들어 버림
-            int y = chickens.get(i).y;
-            int x = chickens.get(i).x;
-            arr[y][x] = 0;
-        }
-        
-        // 최종 프린트문
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(arr[i][j]);
-            }
-            System.out.println();
-        }
-
-        calculateFinal();
+        visited = new boolean[chickens.size()];
+        chooseChicken(0, 0);
+        System.out.println(finalSum);
     }
 
-    // 3. 그리고 1이 나오면 가장 가까운 2를 찾아서 더한다.
-    public static void calculateFinal() {
-        // 1. 상하좌우 계산하면서 0이면 다 넣고
-        // 2. i,j
-        // 어디까지 가는 최단거리는 bfs
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(arr[i][j]==1){
-                    System.out.println(bfs(i,j));
-                }
-            }
-        }
-    }
+    public static void chooseChicken(int depth, int start) {
+        if (depth == m) {// 폐업시키지 않을 치킨집을 최대 M개 고른 순간!
+            /**
+             *  현재 집의 위치에서 가장 가까운 치킨집의 거리를 구해야 하므로.
+             *  집을 하나 꺼내고, 치킨집을 다돌면서 가장 가까운 값을 min 에 넣고 (min = 한집의 최소 치킨거리)
+             *  min을 모두 더한 값인 sum (=모든 집의 최소 치킨거리 합) sum 중에서도 가장 작은 finalSum 값
+             */
+            int sum=0;
+            for(Location ho: houses){
+                int hoY = ho.y;
+                int hoX = ho.x;
+                int min = Integer.MAX_VALUE;
 
-    public static int bfs(int y, int x) {
-        Queue<Integer[]> q = new LinkedList<>();
-        q.add(new Integer[]{y,x}); // 집일 때 큐에 넣고
-        int dist = Integer.MAX_VALUE;
-        int sum = 0;
-        while(!q.isEmpty()){
-            Integer[] popData = q.poll();
-            int yy = popData[0];
-            int xx = popData[1];
-            System.out.println(" hello");
-            for(int i=0;i<4;i++){
-                int posY = dy[i] + yy;
-                int posX = dx[i] + xx;
-                if((posY>=0 && posY<n) && (posX>=0 && posX<n) && arr[posY][posX] == 2){
-                    sum++;
-                    dist = Math.min(dist, sum);
-                    sum=0;
+                for(int j=0;j<chickens.size();j++){
+                    if(visited[j]) { // 방문한 치킨집 중에서만 계산
+                        int chY = chickens.get(j).y;
+                        int chX = chickens.get(j).x;
+                        min = Math.min(Math.abs(hoY - chY) + Math.abs(hoX - chX), min);
+                    }
                 }
-                if((posY>=0 && posY<n) && (posX>=0 && posX<n) && arr[posY][posX] != 0){
-                    q.add(new Integer[]{posY, posX});
-                }
+                sum += min;
             }
-            sum++;
+            finalSum = Math.min(sum, finalSum);
+            return;
         }
-        System.out.println("나오긴하니");
-        return dist;
-    }
 
-    public static void chooseChicken(){
-        // 치킨집이 나온 경우 그 치킨집 좌표를 calculateDist 로 넘김
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(arr[i][j] == 2){
-                    calculateDist(i,j);
-                }
+        for(int i=start;i<chickens.size();i++){ // 치킨집을 하나씩 방문하기
+            if(!visited[i]) { // 방문하지 않은 치킨집이면
+                visited[i] = true;
+                chooseChicken(depth+1, i+1);
+                visited[i] = false;
             }
         }
-        Collections.sort(chickens, (o1, o2) -> {
-            return o1.min - o2.min;
-        });
-    }
-
-    public static void calculateDist(int y, int x){
-        int sum=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(arr[i][j] == 1){ // 특정 치킨집과 많은 집들간의 거리 합을 구한다.
-                    sum += Math.abs(i-y) + Math.abs(j-x);
-                }
-            }
-        }
-        chickens.add(new Location(y,x,sum)); // 그리고 해당 좌표와 거리의 합을 리스트에 넣음
     }
 }
