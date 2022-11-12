1f6fb535f6825d84952fe46495f30be4840f9e1b
Song-EunJu <jd06280@naver.com>
2022-11-12 14:32:29 +0900

    221112: 부분 수열의 합_dfs

--- src/test.java
+++ src/test.java
@@ -7,30 +7,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/*
+* 최소 1개 이상의 수를 선택해서 그 합이 K가 되는 경우의 수를 구하자
+* 1개씩 방문처리를 하면서 더해가면서 K가 되는 순간 cnt++;
+ */
+
 public class test {
+    static int K;
+    static boolean visited[];
+    static int arr[];
+    static int cnt = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<10;i++){
-            int dump = Integer.parseInt(br.readLine());
-            int arr[] = new int[100];
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0;i<T;i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<100;j++){
+            int N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            visited = new boolean[N];
+            arr = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<N;j++){
                 arr[j] = Integer.parseInt(st.nextToken());
             }
-/* 제일큰놈만 패다가 계속 줄어들다가, 제일 크진 않앗는데 제일 큰놈이 작아지는 바람에 1등이 된 애를 패기시작*/
-            for(int j=0;j<dump;j++){
-                Arrays.sort(arr);
-                arr[0]++;
-                arr[arr.length-1]--;
-            }
-            Arrays.sort(arr);
-            sb.append("#"+(i+1)+" "+(arr[arr.length-1]-arr[0])+"\n");
+            dfs(0, 0);
+            sb.append("#"+(i+1)+" "+cnt+"\n");
+            cnt = 0;
         }
         System.out.println(sb);
+    }
+
+    public static void dfs(int sum, int idx){
+        if(sum == K)
+            cnt++;
+
+        for(int i=idx;i<arr.length;i++){
+            visited[i] = true;
+            dfs(sum + arr[i], i+1);
+            visited[i] = false;
+        }
     }
 }
