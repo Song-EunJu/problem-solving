866993525102b0f819b6c7717ad82019f0144f99
Song-EunJu <jd06280@naver.com>
2023-01-12 11:32:24 +0900

    230112: Sum_배열

--- SW Expert Academy/Sum.java
+++ SW Expert Academy/Sum.java
@@ -1,8 +1,3 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class tt2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
