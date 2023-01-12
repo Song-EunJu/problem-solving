51dafd69a10c02bf072208efb2107170ea24d176
Song-EunJu <jd06280@naver.com>
2023-01-12 18:24:47 +0900

    230112: 스도쿠 검증_배열

--- SW Expert Academy/스도쿠 검증.java
+++ SW Expert Academy/스도쿠 검증.java
@@ -2,7 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+//  위와 같이 겹치는 숫자가 없을 경우 1을 정답으로 출력하고
+//  겹치는 숫자가 있는 경우 0 을 출력
 public class tt {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,32 +36,32 @@ public class tt {
                 }
             }
 
-            //  위와 같이 겹치는 숫자가 없을 경우 1을 정답으로 출력하고
-            //  겹치는 숫자가 있는 경우 0 을 출력
-
             if(cnt != 0) // 겹치는 숫자가 있는 경우 이미 출력했으니까 끝
                 continue;
 
             cnt = 0;
-            // 네모 돌면서
-            for (int k = 0; k < 9; k += 3) {
-                for (int h = k; h < k + 3; h++) {
-                    for (int x = 0; x < 9; x += 3) {
-                        int sum = 0;
-                        for (int j = x; j < x + 3; j++) {
-                            sum += arr[h][j];
+            for(int x=0;x<9;x+=3) {
+                // 3*3 짜리 한 줄
+                for (int h = 0; h < 9; h += 3) {
+                    int sum = 0;
+
+                    // (0,0)~(0,2) ~ (2,0)~(2,2)
+                    for (int k = x; k < x+3; k++) {
+                        for (int j = h; j < h + 3; j++) {
+                            sum += arr[k][j];
                         }
-                        if (sum != 45) {
-                            cnt++;
-                            System.out.println("#" + (i + 1) + " 0");
-                            continue;
-                        }
+                    }
+                    if (sum != 45) {
+                        cnt++;
+                        break;
                     }
                 }
             }
 
             if(cnt == 0) // 겹치는 숫자가 없는 경우
                 System.out.println("#"+(i+1)+" 1");
+            else
+                System.out.println("#"+(i+1)+" 0");
         }
     }
 }
