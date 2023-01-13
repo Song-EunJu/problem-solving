5aee4bf47ef899fc3f22343da1b8001a3f75981d
Song-EunJu <jd06280@naver.com>
2023-01-13 23:51:35 +0900

    230113: 최적 경로_백트래킹+완전 탐색 풀이 추가

--- SW Expert Academy/최적 경로.java
+++ SW Expert Academy/최적 경로.java
@@ -6,6 +6,15 @@ import java.util.Arrays;
 import java.util.List;
 
 // 회사에서 출발하여 N명의 고객을 모두 방문하고 집으로 돌아오는 경로 중 가장 짧은 것만 찾으면 됨
+/**
+ * [완전탐색]
+ *
+ * 1. 회사, 집 좌표 따로 저장
+ * 2. 고객들의 좌표를 배열에 저장 + 해당 배열에 방문여부까지 체크하는 변수 존재
+ * 3. dfs 를 통해 배열 나열하는 경우의 수를 모두 따짐
+ * ++ 이동할 때마다 거리체크한 거를 같이 sum 값으로 넘겨도 괜찮을 듯??
+ *
+ * */
 class Customer {
     int y;
     int x;
@@ -74,5 +83,78 @@ public class test {
                 list.remove(ct);
             }
         }
+    }
+}
+
+
+/** 다른풀이 1
+ * [완전탐색]
+ *
+ * 1. 회사, 집 좌표 따로 저장
+ * 2. 고객들의 좌표를 배열에 저장 + 해당 배열에 방문여부까지 체크하는 변수 존재
+ * 3. dfs 를 통해 배열 나열하는 경우의 수를 모두 따짐
+ * ++ 이동할 때마다 거리체크한 거를 같이 sum 값으로 넘기면 됨
+ * */
+class Pos {
+    int y;
+    int x;
+
+    public Pos(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+}
+public class test {
+    static Pos company;
+    static Pos house;
+    static int N;
+    static int min;
+    static Pos[] customers;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i=0;i<T;i++){
+            min = Integer.MAX_VALUE;
+            N = Integer.parseInt(br.readLine());
+            customers = new Pos[N];
+            visited = new boolean[N];
+            int inputs[] = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt).toArray();
+
+            company = new Pos(inputs[0], inputs[1]);
+            house = new Pos(inputs[2], inputs[3]);
+
+            int k = 4;
+            // 고객 좌표 저장
+            for(int j=4;j<4+N;j++) {
+                customers[j-4] = new Pos(inputs[k], inputs[k+1]);
+                k+=2;
+            }
+            dfs(company.y, company.x, 0, 0); // 회사에서 출발하는 거리 계산하기
+            System.out.println("#"+(i+1)+" "+min);
+        }
+    }
+    public static void dfs(int posY, int posX, int sum, int num){
+        if(num == N){
+            // 회사와 집 간의 거리만 더해줌
+            sum += getDistance(posY, house.y, posX, house.x);
+            min = Math.min(min, sum);
+            return ;
+        }
+
+        for(int i=0;i<N;i++){ // i=idx 이렇게 해버리면, 0부터 다시 방문하는 경우가 빠지게 되니까
+            if(!visited[i]){ // 방문하지 않은 곳만 처리
+                visited[i] = true;
+                int nowY = customers[i].y;
+                int nowX = customers[i].x;
+                dfs(nowY, nowX,sum + getDistance(posY, nowY, posX, nowX), num+1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static int getDistance(int y1, int y2, int x1, int x2){
+        return Math.abs(y1-y2) + Math.abs(x1-x2);
     }
 }
--- src/test.java
+++ src/test.java
@@ -1,78 +1,78 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 // 회사에서 출발하여 N명의 고객을 모두 방문하고 집으로 돌아오는 경로 중 가장 짧은 것만 찾으면 됨
-class Customer {
+/**
+ * [완전탐색]
+ *
+ * 1. 회사, 집 좌표 따로 저장
+ * 2. 고객들의 좌표를 배열에 저장 + 해당 배열에 방문여부까지 체크하는 변수 존재
+ * 3. dfs 를 통해 배열 나열하는 경우의 수를 모두 따짐
+ * ++ 이동할 때마다 거리체크한 거를 같이 sum 값으로 넘기면 됨
+ *
+ * */
+class Pos {
     int y;
     int x;
 
-    public Customer(int y, int x) {
+    public Pos(int y, int x) {
         this.y = y;
         this.x = x;
     }
 }
 public class test {
-    static int companyY, companyX, houseY, houseX;
+    static Pos company;
+    static Pos house;
     static int N;
     static int min;
-    static int customers[][];
-    static List<Customer> list;
+    static Pos[] customers;
+    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         for (int i=0;i<T;i++){
             min = Integer.MAX_VALUE;
             N = Integer.parseInt(br.readLine());
-            list = new ArrayList<>();
-            customers = new int[N][3];
+            customers = new Pos[N];
+            visited = new boolean[N];
             int inputs[] = Arrays.stream(br.readLine().split(" "))
                     .mapToInt(Integer::parseInt).toArray();
 
-            companyY = inputs[0];
-            companyX = inputs[1];
-            houseY = inputs[2];
-            houseX = inputs[3];
+            company = new Pos(inputs[0], inputs[1]);
+            house = new Pos(inputs[2], inputs[3]);
 
             int k = 4;
             // 고객 좌표 저장
             for(int j=4;j<4+N;j++) {
-                customers[j-4][0] = inputs[k];
-                customers[j-4][1] = inputs[k+1];
-                customers[j-4][2] = 0;
+                customers[j-4] = new Pos(inputs[k], inputs[k+1]);
                 k+=2;
             }
-            dfs(0);
+            dfs(company.y, company.x, 0, 0); // 회사에서 출발하는 거리 계산하기
             System.out.println("#"+(i+1)+" "+min);
         }
     }
-    public static void dfs(int num){
+    public static void dfs(int posY, int posX, int sum, int num){
         if(num == N){
-            int sum = 0;
-            sum += Math.abs(companyY-list.get(0).y) + Math.abs(companyX-list.get(0).x);
-            sum += Math.abs(houseY-list.get(N-1).y) + Math.abs(houseX-list.get(N-1).x);
-
-            for(int i=1;i<N;i++)
-                sum += Math.abs(list.get(i).y - list.get(i-1).y) + Math.abs(list.get(i).x - list.get(i-1).x);
-
+            // 회사와 집 간의 거리만 더해줌
+            sum += getDistance(posY, house.y, posX, house.x);
             min = Math.min(min, sum);
             return ;
         }
 
         for(int i=0;i<N;i++){ // i=idx 이렇게 해버리면, 0부터 다시 방문하는 경우가 빠지게 되니까
-            if(customers[i][2] == 0){ // 방문하지 않은 곳만 처리
-                customers[i][2] = 1;
-                Customer ct = new Customer(customers[i][0], customers[i][1]);
-                list.add(ct);
-
-                dfs(num+1);
-
-                customers[i][2] = 0;
-                list.remove(ct);
+            if(!visited[i]){ // 방문하지 않은 곳만 처리
+                visited[i] = true;
+                int nowY = customers[i].y;
+                int nowX = customers[i].x;
+                dfs(nowY, nowX,sum + getDistance(posY, nowY, posX, nowX), num+1);
+                visited[i] = false;
             }
         }
+    }
+
+    public static int getDistance(int y1, int y2, int x1, int x2){
+        return Math.abs(y1-y2) + Math.abs(x1-x2);
     }
 }
