0790eeeed985289c32c6f37f6bce3b4491e2aa32
Song-EunJu <jd06280@naver.com>
2023-01-13 20:50:57 +0900

    230113: 미로2_bfs

---
+++ SW Expert Academy/미로2.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class test {
+    static int startY;
+    static int startX;
+    static int dx[] = {0, 0, 1, -1};
+    static int dy[] = {-1, 1, 0, 0};
+    static boolean visited[][];
+    static int arr[][];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        for (int i=0;i<10;i++){
+            arr = new int[100][100];
+            visited = new boolean[100][100];
+            int num = Integer.parseInt(br.readLine());
+            for(int j=0;j<100;j++) {
+                String s = br.readLine();
+                for(int k=0;k<100;k++){
+                    arr[j][k] = s.charAt(k)-'0';
+                    if(arr[j][k] == 2){
+                        startY = j;
+                        startX = k;
+                    }
+                }
+            }
+            System.out.println("#"+num+" "+bfs(startY, startX));
+        }
+    }
+    public static int bfs(int y, int x){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{y,x});
+
+        while(!q.isEmpty()){
+            int[] xy = q.poll();
+            int nowY = xy[0];
+            int nowX = xy[1];
+            visited[nowY][nowX] = true;
+
+            if(arr[nowY][nowX] == 3)
+                return 1;
+
+            for(int i=0;i<4;i++){
+                int posY = nowY + dy[i];
+                int posX = nowX + dx[i];
+
+                if(posY >=0 && posY <= 99 & posX >=0 && posX <= 99 && visited[posY][posX] == false && arr[posY][posX] !=1){
+                    q.add(new int[] {posY, posX});
+                }
+            }
+        }
+        return 0;
+    }
+}
