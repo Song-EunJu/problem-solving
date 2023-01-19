791bfbd21db32938ca4e3781c0ddc7c74f6521f9
Song-EunJu <jd06280@naver.com>
2023-01-19 20:28:06 +0900

    230119: 데일리실습 6-3.도서 상속 실습_객체 배열 다형성

--- src/test2.java
+++ src/test2.java
@@ -1,63 +1,146 @@
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.Arrays;
+//import java.util.LinkedList;
+//import java.util.Queue;
+//
+////출발지에서 도착지까지 가는 경로 중에 복구 시간이 가장 짧은 경로에 대한 총 복구 시간
+//public class test2 {
+//    static int n;
+//    static int arr[][]; // 원본배열
+//    static int dp[][]; // 수정배열
+//    static boolean visit[][];
+//    static int dy[] = {1, -1, 0, 0};
+//    static int dx[] = {0, 0, 1, -1};
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//        int T = Integer.parseInt(br.readLine());
+//        for (int i=0;i<T;i++) {
+//            n = Integer.parseInt(br.readLine());
+//            arr = new int[n][n]; // 원본 배열
+//            dp = new int[n][n]; // 누적합을 구하는 배열
+//            visit = new boolean[n][n]; // 방문 배열
+//            for(int j=0;j<n;j++){
+//                Arrays.fill(dp[j], Integer.MAX_VALUE);
+//                String s = br.readLine();
+//                for(int k=0;k<n;k++){
+//                    arr[j][k] = s.charAt(k)-'0';
+//                }
+//            }
+//
+//            for(int j=0;j<n;j++){
+//                for(int k=0;k<n;k++){
+//                    System.out.print(arr[j][k]);
+//                }
+//                System.out.println();
+//            }
+//            dp[0][0] = 0;
+//            bfs();
+//            sb.append("#"+(i+1)+" "+dp[n-1][n-1]+"\n");
+//        }
+//        System.out.println(sb);
+//    }
+//
+//    public static void bfs(){
+//        Queue<int[]> q = new LinkedList<>();
+//        q.add(new int[] {0,0});
+//        visit[0][0] = true;
+//
+//        while(!q.isEmpty()){
+//            int now[] = q.poll();
+//            int nowY = now[0];
+//            int nowX = now[1];
+//            visit[nowY][nowX] = true;
+//
+//            for(int k=0;k<4;k++){
+//                int posY = nowY + dy[k];
+//                int posX = nowX + dx[k];
+//
+//                if(posY >= 0 && posY < n && posX >= 0 && posX < n && visit[posY][posX] == false){
+//                    if(dp[posY][posX] > dp[nowY][nowX] + arr[posY][posX]) {
+//                        dp[posY][posX] = dp[nowY][nowX] + arr[posY][posX];
+//                        q.add(new int[]{posY, posX});
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
-//출발지에서 도착지까지 가는 경로 중에 복구 시간이 가장 짧은 경로에 대한 총 복구 시간
 public class test2 {
-    static int n;
-    static int arr[][]; // 원본배열
-    static int dp[][]; // 수정배열
-    static boolean visit[][];
-    static int dx[] = {0, 0, 1, -1};
-    static int dy[] = {1, -1, 0, 0};
-    public static void main(String[] args) throws IOException {
+    static int N,ans;
+    static boolean[][] visited;
+    static int[][] map;
+    static int[][] count;
+    static int[] dx= {-1,1,0,0};
+    static int[] dy= {0,0,-1,1};
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        for (int i=0;i<T;i++) {
-            n = Integer.parseInt(br.readLine());
-            arr = new int[n][n];
-            dp = new int[n][n];
+        int testCase = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= testCase; tc++) {
+            N = Integer.parseInt(br.readLine());
 
-            visit = new boolean[n][n];
-            for(int j=0;j<n;j++){
-                Arrays.fill(dp[j], Integer.MAX_VALUE);
-                String s = br.readLine();
-                for(int k=0;k<n;k++){
-                    arr[j][k] = s.charAt(k)-'0';
+            ans = Integer.MAX_VALUE;
+            map = new int[N][N];
+            count = new int[N][N];
+            visited = new boolean[N][N];
+
+            for (int i = 0; i < N; i++) {
+                String temp = br.readLine();
+                for (int j = 0; j < N; j++) {
+                    map[i][j] = temp.charAt(j)-'0';
                 }
             }
-            dp[0][0] = 0;
-            bfs(0, 0);
-            sb.append("#"+(i+1)+" "+dp[n-1][n-1]+"\n");
+            for (int i = 0; i < N; i++) {
+                Arrays.fill(count[i], Integer.MAX_VALUE);
+            }
+            count[0][0] = 0;
+            bfs();
+            sb.append("#"+tc+" "+ans+"\n");
         }
         System.out.println(sb);
     }
+    private static void bfs() {
+        Queue<point> q = new LinkedList<>();
+        q.add(new point(0,0));
+        visited[0][0] = true;
+        while(!q.isEmpty()) {
+            point temp = q.poll();
 
-    public static void bfs(int i, int j){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {i,j});
+            if(temp.x ==N-1 && temp.y==N-1) {
+                ans = ans > count[N-1][N-1]?count[N-1][N-1]:ans;
+            }
 
-        while(!q.isEmpty()){
-            int nowY = q.peek()[0];
-            int nowX = q.peek()[1];
-            visit[nowY][nowX] = true;
-            q.poll();
-
-            for(int k=0;k<4;k++){
-                int posY = nowY + dy[k];
-                int posX = nowX + dx[k];
-
-                if(posY >= 0 && posY < n && posX >= 0 && posX < n && visit[posY][posX] == false){
-                    if(dp[posY][posX] >= dp[nowY][nowX] + arr[posY][posX]) {
-                        dp[posY][posX] = dp[nowY][nowX] + arr[posY][posX];
-                        q.add(new int[]{posY, posX});
-                    }
+            for (int k = 0; k < 4; k++) {
+                int xx = temp.x + dx[k];
+                int yy = temp.y + dy[k];
+                if(xx<0||xx>=N||yy<0||yy>=N)continue;
+                if(!visited[xx][yy] || count[xx][yy]> count[temp.x][temp.y]+map[xx][yy]) {
+                    visited[xx][yy] = true;
+                    count[xx][yy] = count[temp.x][temp.y]+map[xx][yy];
+                    q.offer(new point(xx, yy));
                 }
             }
+        }
+    }
+
+    static class point{
+        int x;
+        int y;
+        public point(int x, int y) {
+            super();
+            this.x = x;
+            this.y = y;
         }
     }
 }
