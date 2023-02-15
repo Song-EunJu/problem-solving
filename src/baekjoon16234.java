8a5ea4307ee2388715496871b3f21a020b6caa02
Song-EunJu <jd06280@naver.com>
2023-02-16 01:39:14 +0900

    230215: 인구 이동_bfs

--- src/baekjoon16234.java
+++ src/baekjoon16234.java
@@ -25,7 +25,6 @@ public class baekjoon16234 {
         L = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
         arr = new int[N][N];
-        visit = new boolean[N][N];
 
         for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
@@ -36,12 +35,14 @@ public class baekjoon16234 {
 
         int cnt=0;
         while(true){
+            visit = new boolean[N][N]; // 재분배한번 끝나고 나서
+
             List<List<Pos>> list = new ArrayList<>();
             for(int i=0;i<N;i++){
                 for(int j=0;j<N;j++){
-                    if(!visit[i][j]) {
+                    if(visit[i][j]==false) {
                         List<Pos> posList = bfs(i, j);
-                        if(posList.size()!=0 && posList.size()!=1)
+                        if(posList.size() != 1)
                             list.add(posList);
                     }
                 }
@@ -53,19 +54,13 @@ public class baekjoon16234 {
             // 국경선이 모두 열린 경우 인구이동 시작
             redistribution(list);
 
-            for(int i=0;i<N;i++){
-                for(int j=0;j<N;j++){
-                    System.out.print(arr[i][j]+" ");
-                }
-                System.out.println();
-            }
             cnt++;
         }
         System.out.println(cnt);
     }
 
     public static List<Pos> bfs(int y, int x){
-        List<Pos> list = new ArrayList<>();
+        List<Pos> list = new ArrayList<>(); // 한 국가에서 연결된 모든 국가들의 좌표를 담는 리스트
         Queue<Pos> q = new LinkedList<>();
         q.add(new Pos(y,x));
 
@@ -73,16 +68,18 @@ public class baekjoon16234 {
             Pos now = q.poll();
             int nowY = now.y;
             int nowX = now.x;
+            if(visit[nowY][nowX] == true)
+                continue;
             visit[nowY][nowX] = true;
             list.add(now);
 
             for (int i = 0; i < 4; i++) {
                 int posY = nowY + dy[i];
                 int posX = nowX + dx[i];
-                if (posY >= 0 && posY < N && posX >= 0 && posX < N && !visit[posY][posX]) {
-                    // 범위에도 들어가고, 방문하지 않은 경우
+
+                if (posY >= 0 && posY < N && posX >= 0 && posX < N) {
                     int diff = Math.abs(arr[nowY][nowX] - arr[posY][posX]);
-                    if (L <= diff && diff <= R) { // 두 나라의 인구 차이가 범위 내에 들어간다면
+                    if ((L <= diff && diff <= R) && visit[posY][posX]==false) { // 두 나라의 인구 차이가 범위 내에 들어간다면 해당 인접국가도 큐에 넣음
                         q.add(new Pos(posY, posX));
                     }
                 }
@@ -92,17 +89,14 @@ public class baekjoon16234 {
     }
 
     public static void redistribution(List<List<Pos>> list){
-        int sum = 0;
-        for(int i=0;i<list.size();i++){ // 연합인구 그룹
-            List<Pos> population = list.get(i);
+        for(int i=0;i<list.size();i++){ // 연합인구 그룹이 몇개인지
+            int sum = 0;
+            List<Pos> population = list.get(i); // 해당 그룹 내의 국가가 몇개인지
             for(int j=0;j<population.size();j++){
                 int y = population.get(j).y;
                 int x = population.get(j).x;
                 sum += arr[y][x];
             }
-
-            if(population.size()==0)
-                continue;
 
             int divide = sum/population.size();
             for(int j=0;j<population.size();j++){
