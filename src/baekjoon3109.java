217e9d09ba9e45c6ab1be832d442bf33565d0d09
Song-EunJu <jd06280@naver.com>
2023-02-23 15:04:13 +0900

    230223: 빵집_백트래킹

--- src/baekjoon3109.java
+++ src/baekjoon3109.java
@@ -25,13 +25,6 @@ public class baekjoon3109 {
             }
         }
 
-        /**
-         1번 경로에서 나올 수 있는게 a,b,c 경로가 있다고 가정
-         -> a경로를 선택했을 때 뒤에서 1개만 더 선택할 수 있다고 가정
-         -> 만약에 b 경로를 선택했다면 뒤에서 3개를 더 선택할 수 있었다면 ?
-         -> 그니까 하나의 경로를 선택한 다음에는, y 인덱스 다음 지점으로 돌아가서 다시 뽑는 식으로 진행하려고 했는데!
-         -> 그러면 최단 경우의수를.. 못 구 하는.. ?
-         */
         for(int i=0;i<R;i++) { // R번만큼 돌면서 시작지점은 계속 바꿔줘야 함
             start(i, 0);
         }
@@ -42,10 +35,17 @@ public class baekjoon3109 {
     /**
      * 사고 정리가 안됨.
      *
-     * (1) 그리디인가? 라는 생각을 한 이유
+     * 1번 경로에서 나올 수 있는게 a,b,c 경로가 있다고 가정
+     *          -> a경로를 선택했을 때 뒤에서 1개만 더 선택할 수 있다고 가정
+     *          -> 만약에 b 경로를 선택했다면 뒤에서 3개를 더 선택할 수 있었다면 ?
+     *          -> 그니까 하나의 경로를 선택한 다음에는, y 인덱스 다음 지점으로 돌아가서 다시 뽑는 식으로 진행하려고 했는데!
+     *          -> 그러면 최단 경우의수를.. 못 구 하는.. ?
+     *
+     * 그렇다면 그리디인가?
      * -> 갈림길이 여러개 나오는 경우, 그 순간의 최선의 선택을 하여 최댓값을 구해야 함
      * -> 근데 그 순간에 최선인지 아닌지 어떻게 알지? 무조건 안 겹치게 오른쪽 상단으로 올려야 하는 건가...
-     *
+     * [결론] 오른쪽 상단부터 가는 게 맞았음. 오른쪽 상단 - 오른쪽 - 오른쪽 아래 순서대로 for문 돌면서 파이프라인 생성되면
+     *          바로 true 처리하고 다시 맨 첫번째좌표로 돌아가서 새로운 파이프라인을 찾도록 함
      * */
     public static boolean start(int y, int x){
         if(x==C-1) { // 마지막 열에 도착한 경우
@@ -53,30 +53,17 @@ public class baekjoon3109 {
             return true;
         }
 
+        int nowX = x+1;
         for(int i=0;i<3;i++){
-            int nowY = y+dy[i];
-            int nowX = x+1;
+            int nowY = y+dy[i]; // 탐색의 순서가 오른쪽 위 - 오른쪽 - 오른쪽 아래로 해야 최적 경로 찾음
 
-//            // 범위 밖이라면 연산 안함
-//            if (!(nowY >= 0 && nowY < R) || !(nowX >= 0 && nowX < C)) continue;
-//
-//            // 건물이 있는 경우에는 연산 안함
-//            if (bread[nowY][nowX] == 'x') continue;
-
-            // 방문 표시
-            visit[nowY][nowX] = true;
-
-            // nowX 를 들고 다음 열 검사
-            //라인의 가지치기 ( 불필요한 부분을 끝까지 탐색하지 않는 것 )를 방지하기 위해 true를 리턴한다.
-            //true를 리턴하면 다음 분기문으로 넘어가지 않는다.
-            if (start(nowY,nowX)) return true;
-
-//            if(promising(nowY, nowX)){
-//                visit[nowY][nowX] = true;
-//                start(nowY, nowX);
-//                return true;
-////                break; // 갈림길 여러개 나오는 경우에 하나 true 처리하면 걔는 더이상 안가도록 break
-//            }
+            if(promising(nowY, nowX)){
+                visit[nowY][nowX] = true;
+                if(start(nowY, nowX))
+                    // 마지막 열에 도착해서 파이프생성이 true 인 경우에는, 다른 갈림길이 가능하더라도 가지 않기 위해 true 를 리턴
+                    return true;
+                /** 여기서 break 하면 안되는 경우 : return false 하게 되니까 갈림길을 계속 탐방하게 됨*/
+            }
         }
         return false;
     }
