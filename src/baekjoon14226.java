eb8fc2ad9e5d7537c1a63a501c0a502add22c71b
Song-EunJu <jd06280@naver.com>
2023-03-11 01:52:35 +0900

    230311: 게리맨더링_bfs/부분집합

---
+++ src/baekjoon14226.java
@@ -0,0 +1,12 @@
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.util.StringTokenizer;
+//
+//public class baekjoon14226 {
+//    public static void main(String[] args) {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int A = Integer.parseInt(st.nextToken());
+//        int B = Integer.parseInt(st.nextToken());
+//    }
+//}
--- src/baekjoon17471.java
+++ src/baekjoon17471.java
@@ -1,24 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
+// 구역을 2개로 나누기 -> 깔끔하게 나누는 방법을 알고 싶음.
+// 그 구역 2개가 다 연결되어있는지 체크 -> bfs
+// 연결되어 있다면 인구의 합이 최소
 public class baekjoon17471 {
     static int N;
     static boolean visit[];
     static int population[];
     static List<Integer>[] map;
+    static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
-        // 구역을 두 개의 선거구로 나눠야 하고,
-        // 각 구역은 두 선거구 중 하나에 포함되어야 한다.
-        // 선거구는 구역을 적어도 하나 포함해야 하고, 한 선거구에 포함되어 있는 구역은 모두 연결되어 있어야 한다.
-        // 구역 A에서 인접한 구역을 통해서 구역 B로 갈 수 있을 때, 두 구역은 연결되어 있다고 한다.
-        // 중간에 통하는 인접한 구역은 0개 이상이어야 하고, 모두 같은 선거구에 포함된 구역이어야 한다.
-
-        // 두 선거구에 포함된 인구차이를 최소로 하기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine()); // 구역 개수
         visit = new boolean[N+1];
@@ -41,37 +36,77 @@ public class baekjoon17471 {
                 map[i].add(Integer.parseInt(st.nextToken()));
             }
         }
-
         divideArea(0);
-        // 구역을 2개로 나누기
-        // 그 구역 2개가 다 연결되어있는지 체크
-        // 연결되어 있다면 인구의 합이 최소
+        System.out.println(min == Integer.MAX_VALUE ? -1 : min);
     }
 
-    // 적어도 1개는 포함하도록
     private static void divideArea(int cnt) {
         if(cnt == N){
-            // 다 true 이거나 다 false 인 경우는 제외 해야 함.
+            /**
+             * [부분집합을 2개로 나누기]
+             * 1. boolean형 visit 배열 T/F 로 나눠서 리스트에 넣기
+             * 2. int형 배열에 1,2 와 같은 데이터로 구분하기
+             * ex) area[k] = 1;
+             *     dfs(k + 1);
+             *     area[k] = 2;
+             *     dfs(k + 1);
+             */
+            List<Integer> listA = new ArrayList<>();
+            List<Integer> listB = new ArrayList<>();
 
-            System.out.print("첫번째 집합 : ");
+            int populationA = 0;
+            int populationB = 0;
             for(int i=0;i<N;i++){
-                if(visit[i] == true){
-                    System.out.print((i+1)+" ");
+                if(visit[i]) {
+                    listA.add(i + 1);
+                    populationA += population[i+1];
+                }
+                else {
+                    listB.add(i + 1);
+                    populationB += population[i+1];
                 }
             }
-            System.out.println();
-            System.out.print("두번째 집합 : ");
-            for(int i=0;i<N;i++){
-                if(visit[i] == false){
-                    System.out.print((i+1)+" ");
-                }
-            }
-            System.out.println();
+
+            // 다 true 이거나 다 false 인 경우는 제외 해야 함.
+            if(listA.size() == 0 || listB.size() == 0)
+                return;
+
+            if(check(listA) && check(listB)) // 2개가 다 연결되어 있다면
+                min = Math.min(min, Math.abs(populationA-populationB));
+
             return;
         }
         visit[cnt] = true;
         divideArea(cnt+1);
         visit[cnt] = false;
         divideArea(cnt+1);
+    }
+    /**
+     * 뽑은 좌표들이 모두 연결되어 있는지를 어떻게 BFS 로 어떻게 확인하는지 감이 안잡혔음
+     * */
+    private static boolean check(List<Integer> list) {
+        boolean visitInCheck[] = new boolean[N+1];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(list.get(0));
+        visitInCheck[list.get(0)] = true;
+        int cnt = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            /** A 리스트에 속한 요소를 하나씩 꺼내고
+             * 원래 전체 구역 내에서 해당 요소랑 인접한 노드들을 꺼낸다
+             * 이 때, 인접한 노드가 A 리스트에 속했는데 방문하지 않은 경우에만 큐에 다시 넣는다
+             * **/
+            for(int num : map[node]){
+                if(list.contains(num) && visitInCheck[num] == false){
+                    q.add(num);
+                    visitInCheck[num] = true;
+                }
+            }
+            cnt++;
+        }
+        // 큐에 넣은 갯수가 A 리스트의 크기랑 동일하면 모두 연결된 그래프인 것
+        if(cnt == list.size())
+            return true;
+        return false;
     }
 }
